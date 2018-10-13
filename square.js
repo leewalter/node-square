8454047a875231d7e7207502d424ea54e0827204
Walter Lee <quietwalter@gmail.com>
2018-10-12 21:20:57 -0700

    test string

--- square.js
+++ square.js
@@ -1,9 +1,10 @@
-module.exports = x => {
-    const xx = x ** 2;
-    const xxx = xx * x; 
+module.exports = resp1 => {
+//    const xx = x ** 2;
+//    const xxx = xx * x; 
     console.log(`Walter Lee first Knative serverless function with riff !`);
     console.log(`riff = Function as a Service (FaaS) for Kubernetes. The riff project builds on top of the Knative project’s build, serving and eventing features.`);
     console.log(`the square of ${x} is ${xx}`);
     console.log(`the cube of ${x} is ${xxx}`);
-    return (`the square and cube of ${x} are: ${xx}, ${xxx}`);
+//    return (`the square and cube of ${x} are: ${xx}, ${xxx}`);
+    return (`Walter Lee first Knative serverless function with riff !`);
 }
